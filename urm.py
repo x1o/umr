436b2b052ac7b13fb5351027b6c22f5910ff22d1
Dmitry Zotikov <xio@ungrund.org>
2014-09-08 19:57:21 +0400

    die more verbosely

--- urm.py
+++ urm.py
@@ -21,29 +21,37 @@ line_n = 0
 # memory[1] = 'a'
 # memory[2] = 'b'
 
+def die(msg, code=0):
+    print(msg)
+    exit(code)
+
 with open(source_filename, 'r') as source_file:
-    source = source_file.readlines()
+    source = [line for line in source_file.readlines() if not line.startswith('#')]
     while line_n < len(source):
         expression = source[line_n].lstrip()
         command = expression[0].lower()
         print(' - %s - ' % line_n)
-        if command == 'z':
-            register = int(expression[2:])
-            memory[register] = 0
-            line_n += 1
-        elif command == 's':
-            register = int(expression[2:])
-            memory[register] += 1
-            line_n += 1
-        elif command == 't':
-            r1, r2 = [int(r) for r in expression[2:].split(',')]
-            memory[r2] = memory[r1]
-            line_n += 1
-        elif command == 'j':
-            r1, r2, n = [int(r) for r in expression[2:].split(',')]
-            if memory[r1] == memory[r2]:
-                line_n = n
-            else:
+        try:
+            if command == 'z':
+                register = int(expression[2:])
+                memory[register] = 0
                 line_n += 1
-        else:
-            raise SyntaxError('unknown command %s, line %s' % (command, line_n))
+            elif command == 's':
+                register = int(expression[2:])
+                memory[register] += 1
+                line_n += 1
+            elif command == 't':
+                r1, r2 = [int(r) for r in expression[2:].split(',')]
+                memory[r2] = memory[r1]
+                line_n += 1
+            elif command == 'j':
+                r1, r2, n = [int(r) for r in expression[2:].split(',')]
+                if memory[r1] == memory[r2]:
+                    line_n = n
+                else:
+                    line_n += 1
+            else:
+                raise SyntaxError('unknown command %s, line %s' % (command, line_n))
+        except KeyError:
+            die('an uninitialized register has been referenced')
+    die('eof')
